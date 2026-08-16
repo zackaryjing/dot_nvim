410f937cd860c7e3174e93be5e3a3493301740e8
zackary jing <zackary.jing@gmail.com>
2026-08-16 21:15:31 +0800

    feat: refine terminal, snippets, and surround workflow

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -3,6 +3,7 @@ return {
     "folke/flash.nvim",
     keys = {
       { "s", false, mode = { "n", "x", "o" } },
+      { "S", false, mode = { "n", "x", "o" } },
       { "<leader>j", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     },
   },
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,6 +2,11 @@ return {
   {
     "folke/snacks.nvim",
     opts = {
+      terminal = {
+        win = {
+          height = 10,
+        },
+      },
       picker = {
         sources = {
           explorer = {
